2e4e692087aca9a4336e670ca4afd5aa36e90b70
takeshi.hisatomi <take_tomy7@yahoo.co.jp>
2024-10-12 22:52:20 +0900

    kadai-java-review03

--- src/baseball/Review03.java
+++ src/baseball/Review03.java
@@ -4,19 +4,19 @@ public class Review03 {
 
     public static void main(String[] args) {
                                         // (チーム名、勝ち、負け、引き分け）
-       BaseballTeam Yakuruto = new BaseballTeam("東京ヤクルトスワローズ", 80, 59, 4);
-       BaseballTeam Baystars = new BaseballTeam("横浜DeNAベイスターズ", 73, 68, 2);
-       BaseballTeam Tigers = new BaseballTeam("阪神タイガース", 68, 71, 4);
-       BaseballTeam Giants = new BaseballTeam("読売ジャイアンツ", 68, 72, 3);
-       BaseballTeam Carp = new BaseballTeam("広島東洋カープ", 66, 74, 3);
-       BaseballTeam Dragons = new BaseballTeam("中日ドラゴンズ", 66, 75, 2);
+       BaseBallTeam Yakuruto = new BaseBallTeam("東京ヤクルトスワローズ", 80, 59, 4);
+       BaseBallTeam Baystars = new BaseBallTeam("横浜DeNAベイスターズ", 73, 68, 2);
+       BaseBallTeam Tigers = new BaseBallTeam("阪神タイガース", 68, 71, 4);
+       BaseBallTeam Giants = new BaseBallTeam("読売ジャイアンツ", 68, 72, 3);
+       BaseBallTeam Carp = new BaseBallTeam("広島東洋カープ", 66, 74, 3);
+       BaseBallTeam Dragons = new BaseBallTeam("中日ドラゴンズ", 66, 75, 2);
 
-       Yakuruto.report();
-       Baystars.report();
-       Tigers.report();
-       Giants.report();
-       Carp.report();
-       Dragons.report();
+       Yakuruto.report(); // ヤクルトのreportメソッドを呼び出し
+       Baystars.report(); // ベイスターズのreportメソッドを呼び出し
+       Tigers.report(); // タイガースのreportメソッドを呼び出し
+       Giants.report(); // ジャイアンツのreportメソッドを呼び出し
+       Carp.report(); // カープのreportメソッドを呼び出し
+       Dragons.report(); // ドラゴンズのreportメソッドを呼び出し
     }
 
 }
